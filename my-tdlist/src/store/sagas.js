d944d24bc2479cda4d1d1ddc2d6fcb65f48088e3
maoyining <2044964754@qq.com>
2020-05-11 20:45:07 +0800

    saga catch err

--- my-tdlist/src/store/sagas.js
+++ my-tdlist/src/store/sagas.js
@@ -3,9 +3,13 @@ import { GET_INIT_LIST } from './actionType';
 import { initListAction } from './actionCreator';
 import axios from 'axios'
 function* getDate(){
-  const res=yield axios.get('/todolist.json')
-  const action=initListAction(res.data)
-  yield put(action)
+  try{
+    const res=yield axios.get('/todolist.json')
+    const action=initListAction(res.data)
+    yield put(action)
+  }catch(e){
+    console.log("error")
+  }
 }
 function* mySaga() {
   yield takeEvery(GET_INIT_LIST,getDate)
